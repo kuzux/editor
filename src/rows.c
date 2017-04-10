484992b552d06cd90ab93da5f02b595b9113e56a
Arda Cinar <arda@cinar.be>
2017-04-10 12:53:03 +0300

    Starting chapter 5

--- src/rows.c
+++ src/rows.c
@@ -4,6 +4,85 @@
 #include <fled/common.h>
 #include <fled/rows.h>
 
+/** 
+ * rows_t is essentially a dynamically sized array of
+ * row_t elements. It doubles or halves in size if the size 
+ * changes enough. The implementation is pretty straightforward as well.
+ * In the future, we might make a more efficient data structure
+ * maybe with links that only contain the line number and an actual array
+ * structure only for the lines actually displayed on screen. However,
+ * that might make search or syntax highlighting operations costly.
+ */
+
+/**
+ * Similarly, row_t is a dynamically sized character array. We can
+ * assume that each row is a short-ish one and have less of a performace 
+ * issue. The structure actually has two array structures, one for the source  
+ * line and one for the rendered line. We might need to make it a rows_t like 
+ * structure to support resizing
+ * TODO: Separate it to another file
+ */
+
+/* Insert a new character. Takes only one character and the idx
+ * parameter is a source line index.
+ * TODO: Don't do this for every character
+ */
+void row_insertchar(row_t* row, int at, int c) {
+    if(at < 0){
+        return;
+    }
+
+    if(at > row->len) {
+        at = row->len;
+    }
+
+    int newlen = row->len+1;
+    int newrlen;
+
+    if(c == '\t') {
+        newrlen = row->rlen+1;
+
+        while(newrlen % EF->config.tabstop != 0) {
+           newrlen++;
+        }
+    } else {
+        newrlen = row->rlen+1;
+    }
+
+    int renderat = 0;
+
+    int i;
+    for(i = 0; i<at; i++) {
+        if(row->buf[i] == '\t'){
+            renderat++;
+            while(renderat % EF->config.tabstop != 0) {
+                renderat++;
+            }
+        } else {
+            renderat++;
+        }
+    }
+
+    row->buf = realloc(row->buf, newlen);
+    row->len++;
+    row->rbuf = realloc(row->rbuf, newrlen);
+    row->rlen = newrlen;
+
+    memmove(&row->buf[at + 1], &row->buf[at], row->len - at);
+    memmove(&row->rbuf[renderat + 1], &row->rbuf[renderat], row->rlen - renderat);
+
+    row->buf[at] = c;
+    if(c == '\t') {
+        row->rbuf[renderat++] = ' ';
+
+        while(renderat % EF->config.tabstop != 0) {
+            row->rbuf[renderat++] = ' ';
+        }
+    } else {
+        row->rbuf[renderat] = c;
+    }
+}
+
 rows_t* make_rows() {
     rows_t* res;
     res = (rows_t*)malloc(sizeof(rows_t));
